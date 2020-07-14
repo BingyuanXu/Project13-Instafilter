ec121071f7bc5b599b3dc224d0e8e0baea8ee642
BingyuanXu <xubingyuan123@hotmail.com>
2020-07-14 08:16:29 -0500

    make custom binding to bind slider to filter intensity

--- Project13Instafilter/ContentView.swift
+++ Project13Instafilter/ContentView.swift
@@ -7,22 +7,52 @@
 //
 
 import SwiftUI
+import CoreImage
+import CoreImage.CIFilterBuiltins
 
 struct ContentView: View {
   @State private var image: Image?
   @State private var filterIntensity = 0.5
   @State private var showImagePicker = false
-  @State private var imputImage: UIImage?
+  @State private var inputImage: UIImage?
+  @State private var currentFilter = CIFilter.sepiaTone()
+  let context = CIContext();
+  
+  
+  func applyProcessing() {
+    currentFilter.intensity = Float(filterIntensity)
+    
+    guard let outputImage = currentFilter.outputImage else { return }
+    
+    if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
+      let uiImage = UIImage(cgImage: cgimg)
+      image = Image(uiImage: uiImage)
+    }
+  }
+  
   
   func loadImage() {
-    guard let inputImage = imputImage else { return }
+    guard let inputImage = inputImage else { return }
     
-    image = Image(uiImage: inputImage)
+    let beginImage = CIImage(image: inputImage)
+    currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
     
+    applyProcessing()
   }
   
   var body: some View {
-    NavigationView {
+    
+    let intensity = Binding<Double>(
+      get: {
+        self.filterIntensity
+    },
+      set: {
+        self.filterIntensity = $0
+        self.applyProcessing()
+    }
+    )
+    
+    return NavigationView {  //Now that there is some logic inside the body property, you must place return
       VStack {
         ZStack {
           Rectangle()
@@ -45,7 +75,7 @@ struct ContentView: View {
         
         HStack {
           Text("Intensity")
-          Slider(value: self.$filterIntensity)
+          Slider(value: intensity)
         }
         .padding(.vertical)
         
@@ -64,7 +94,7 @@ struct ContentView: View {
       .padding([.leading, .bottom, .trailing])
       .navigationBarTitle("Instafilter")
       .sheet(isPresented: $showImagePicker, onDismiss: loadImage){
-        ImagePicker(image: self.$imputImage)
+        ImagePicker(image: self.$inputImage)
       }
     }
   }
--- Project13Instafilter/ImagePicker.swift
+++ Project13Instafilter/ImagePicker.swift
@@ -18,7 +18,7 @@ struct ImagePicker: UIViewControllerRepresentable {
   
   class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
     let parent: ImagePicker
-      
+    
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
       if let uiImage = info[.originalImage] as? UIImage {
         parent.image = uiImage
